ba57b5291e879285a368bfa3a21b3f44df4ea2da
Olha Mushket <olhamushket@gmail.com>
2022-06-28 23:22:50 +0300

    Solved

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -11,9 +11,21 @@ public class QuadraticEquation {
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
-
-
-
+        double d, x1, x2;
+        d=b*b - 4*a*c;
+        if (d<0) {
+            System.out.println("no roots");
+        } else {
+            if (d==0) {
+                x1=-b/(2*a);
+                System.out.println(x1);
+            } else {
+                d=Math.sqrt(d);
+                x1=(-b+d)/(2*a);
+                x2=(-b-d)/(2*a);
+                System.out.println(x1 +" "+x2);
+            }
+        }
     }
 
 }
